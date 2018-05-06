6acca0f056b73552686c7c68d43ad201b3e803a9
Grant Barchard <gbarchard@c-24-72-228-99.ni.gigamonster.net>
2018-05-06 14:53:21 -0400

    Fixed toggling of functions on button click

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function myFunction() {
     var NAME = document.getElementById("MemeButton")
     NAME.className="GoodMeme"
+    NAME.onclick =  function() {SmallButton()}
+
   }
+
+function SmallButton () {
+    var NAME = document.getElementById("MemeButton")
+    NAME.className= "BadMeme"
+    NAME.onclick = function() {myFunction()}
+
+}
